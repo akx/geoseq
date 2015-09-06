e2734d4dea4159a58d825d4c3ce5add72b3783bb
Aarni Koskela <akx@iki.fi>
2015-09-06 23:19:27 +0300

    More samples

--- src/audio/index.js
+++ src/audio/index.js
@@ -3,7 +3,12 @@ const Sample = require("./sample");
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 const context = new AudioContext();
 export var samples = {
-    "click": new Sample(context, require("!url!./tick.wav"))
+    "click": new Sample(context, require("!url!./tick.wav")),
+    "marimba": new Sample(context, require("!url!./micromarimba.wav")),
+    "piano": new Sample(context, require("!url!./piano.wav")),
+    "808kick": new Sample(context, require("!url!./808kick.wav")),
+    "909snare": new Sample(context, require("!url!./909snare.wav")),
+    "909chh": new Sample(context, require("!url!./909chh.wav")),
 };
 
 export function play(soundName, pitch = 1) {
